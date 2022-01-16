e45cfea996cf829ca0f4804ee24f848ad4a0969a
Sampad Sarker <sampadsarker777@gmail.com>
2022-01-16 15:03:04 +0600

    uploaded

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,17 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <AddTask @taskWithPriority="taskAndPriority"></AddTask>
   <TaskList :getTask="getTask"></TaskList>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
 import AddTask from "../src/components/AddTask.vue";
 import TaskList from "../src/components/TaskList.vue";
-// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
     AddTask,
     TaskList,
-    // HelloWorld,
   },
   emits: ["taskWithPriority"],
 
@@ -27,12 +24,11 @@ export default {
   },
   methods: {
     taskAndPriority(taskWithPriority) {
-      console.log("in App:", taskWithPriority);
+      // console.log("in App:", taskWithPriority);
       // this.getTask.push(taskWithPriority);
 
       [this.getTask] = [taskWithPriority];
-      console.log("getTask in app", this.getTask);
-
+      // console.log("getTask in app", this.getTask);
 
       // this.getTaskList.reverse();
     },
